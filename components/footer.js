1448d5d71a5c0090e074f52e0fd108bfe38d908b
srikanth <chiramanasrikanth265@gmail.com>
2022-02-25 10:24:36 +0530

    complete landing page

---
+++ components/footer.js
@@ -0,0 +1,82 @@
+function footer() {
+  return `
+     <h3>Cities We deliver to</h3>
+			<div id="cities">
+				<div class="cities-1">
+					<p>Pune</p>
+					<p>Gurgaon</p>
+					<p>Indore</p>
+					<p>Ghaziabad</p>
+					<p>Bhopal</p>
+					<p>Pondicherry</p>
+					<p>Mohali</p>
+					<p>Zirakpur</p>
+					<p>Tirupati</p>
+				</div>
+				<div class="cities-2">
+					<p>Mumbai</p>
+					<p>New Delhi</p>
+					<p>Nagpur</p>
+					<p>Coimbatore</p>
+					<p>Kolkata</p>
+					<p>Nashik</p>
+					<p>Chandigarh</p>
+					<p>Lucknow</p>
+					<p>Tiruppur</p>
+				</div>
+				<div class="cities-3">
+					<p>Banglore</p>
+					<p>Hyderabad</p>
+					<p>Faridabad</p>
+					<p>Dubai</p>
+					<p>Surat</p>
+					<p>Jaipur</p>
+					<p>Vijaywada</p>
+					<p>Kochi</p>
+					<p>Kota</p>
+				</div>
+				<div class="cities-4">
+					<p>Noida</p>
+					<p>Chennai</p>
+					<p>Ahmedabad</p>
+					<p>Vadodra</p>
+					<p>Mysore</p>
+					<p>Panchukla</p>
+					<p>Vellore</p>
+					<p>Vizag</p>
+					<p>Many more...</p>
+				</div>
+			</div>
+
+			<div id="legal-contact">
+				<div id="legal">
+					<h3>Legal</h3>
+					<p>Terms & Conditions</p>
+					<p>Privacy Policy</p>
+				</div>
+
+				<div id="contact">
+					<h3>Contact</h3>
+					<p><i class="fa-solid fa-envelope"></i> crm@faasos.com</p>
+				</div>
+			</div>
+
+			<hr class="sep" />
+
+			<div class="bottom">
+				<div>
+					<p>2020 &copy; Faasos. All rights reserved</p>
+				</div>
+				<div>
+					<h3>Follow us on</h3>
+					<div class="icons">
+						<span><i class="fa-brands fa-facebook-f social-icon"></i></span>
+						<span><i class="fa-brands fa-twitter social-icon"></i></span>
+						<span><i class="fa-brands fa-instagram social-icon"></i></span>
+					</div>
+				</div>
+			</div>
+  `;
+}
+
+export default footer;
