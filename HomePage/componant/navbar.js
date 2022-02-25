edb65971cf23378c85bbaaa68633e1cf11dca9d1
Primahale <mahalepriyanka82@gmail.com>
2022-02-25 18:21:17 +0530

    done

---
+++ HomePage/componant/navbar.js
@@ -0,0 +1,20 @@
+function navbar(){
+    return ` <div id="left">
+    <div id="logo">
+        <img src="Faasos logo.png" >
+    </div>  
+</div> 
+<div id="right">
+    <div id="profile">
+        <img src="login.png">
+    </div>
+    <div id="name">Login</div>
+    <div id="toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</div>`
+}
+
+export default navbar;
